81e5736f384f7ea21bfbd50e287cfed8333bd90b
Nicole Coleman <cncoleman@stanford.edu>
2015-12-08 09:17:26 -0800

    Move old dev to another branch. Starting Fresh

--- gulp/watch.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', function(){
-  gulp.watch('./src/**/*.jade', ['partials']);
-  gulp.watch('./src/**/*.js', ['scripts']);
-  gulp.watch('./src/**/*.styl', ['styles']);
-  gulp.watch('./src/index.jade', ['index']);
-})
