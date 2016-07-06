9b16e08f8232b2686efae9b5db8ab510aa59e3a8
Ethan Jewett <esjewett@gmail.com>
2016-07-06 12:47:09 -0500

    Bring in D3v4, very basic visual display and wiring of click event back into the component

--- app/scripts/explore-component.ts
+++ app/scripts/explore-component.ts
@@ -1,13 +1,27 @@
 namespace fibra {
   'use strict'
 
-  class SparqlExploreComponentController {
+  interface WindowServiceWithD3 extends angular.IWindowService {
+    d3: any
+  }
+
+  interface ExploreComponentInterface extends angular.IComponentController {
+
+  }
+
+  class ExploreComponentController {
     public itemService: SparqlItemService
     public items: Item[]
     public selectedItem: INode
     public properties: {}[]
     public classTreePromise: angular.IPromise<TreeNode[]>
+    private svgSel: any
+    private d3: any
 
+    public qbInitial():void {
+      // A hack until we can get $onInit accessible in the component controller
+      this.$timeout(this.queryAndBuild.bind(this), 3000)
+    }
     public queryAndBuild(): angular.IPromise<String> {
       return this.classTreePromise.then(ct => {
         return this.itemService.getAllItems().then(
@@ -18,8 +32,25 @@ namespace fibra {
             })
             return 'ok'
           }
-        )
+        ).then(this.updateExplore.bind(this))
       })
+    }
+
+    public updateExplore(): string {
+      let items = this.svgSel.selectAll("circle").data(this.items, (d) => { return d.value })
+      items.exit().remove()
+      items.enter().append("circle")
+          .style("fill", "grey")
+        .merge(items)
+          .style("stroke", "grey")
+          .attr("r", "20px")
+          .attr("transform", (d,i) => { return "translate(" + (30+(i*45)) + ",60)" })
+          .on('click', (d:INode) => {
+            this.$scope.$apply(() => {
+              this.selectItem(d)
+            })
+          })
+      return 'ok'
     }
 
     public selectItem(id: INode): void {
@@ -32,14 +63,21 @@ namespace fibra {
       return prom
     }
 
-    constructor(private sparqlItemService: SparqlItemService,
+    constructor(private $element: any,
+                private $window: WindowServiceWithD3,
+                private $scope: angular.IScope,
+                private $timeout: angular.ITimeoutService,
+                private sparqlItemService: SparqlItemService,
                 private fibraService: FibraService,
                 private $q: angular.IQService) {
       this.fibraService.on('change', () => {
         return this.queryAndBuild()
       })
       this.itemService = sparqlItemService
-      this.queryAndBuild()
+      this.svgSel = this.$window.d3.select(this.$element[0]).select('svg')
+      this.d3 = this.$window.d3
+
+      this.qbInitial()
     }
   }
 
@@ -48,7 +86,7 @@ namespace fibra {
       classTreePromise: '<',
       selectedItem: '='
     }
-    public controller: Function = SparqlExploreComponentController
+    public controller: angular.IComponentController = ExploreComponentController
     public templateUrl: string = 'partials/explore.html'   
   }
 }
