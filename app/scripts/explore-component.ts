0a6c85e230cf3b02f26c3e460397b4c458191619
alexsherman <me.alexsherman@gmail.com>
2016-07-12 15:11:35 -0700

    add basic force-directed graph of nodes
    the links right now are arbitrary test links, but the ability is there

--- app/scripts/explore-component.ts
+++ app/scripts/explore-component.ts
@@ -38,40 +38,66 @@ namespace fibra {
     }
 
     public updateExplore(): string {
-      let drag = this.d3.drag()
-        .on("drag", (d,i) => {
-            let x = this.d3.event.x
-            let y = this.d3.event.y
-            //prevents dragging nodes outside of the svg
-            let svg_width = +this.d3.select("#explore").style('width').replace("px", "")
-            let svg_height = +this.d3.select("#explore").style('height').replace("px", "")
-            if (x < 0) x = 0;
-            if (y < 0) y = 0;
-            if (x > svg_width) x = svg_width;
-            if (y > svg_height) y = svg_height;
-          this.d3.select("#node-container-" + i).attr("transform", () => {
-            return "translate(" + [x,y] + ")"
-          })
-     })
+      let svg_width = +this.d3.select("#explore").style('width').replace("px", "")
+      let svg_height = +this.d3.select("#explore").style('height').replace("px", "")
+      let radius = 8
 
-      let items = this.svgSel.selectAll("circle").data(this.items, (d) => {
-        return d.value })
-      items.exit().remove()
+      let demo_links = [
+        {"source": 1, "target": 3},
+        {"source": 4, "target": 2},
+        {"source": 8, "target": 9},
+        {"source": 7, "target": 5},
+        {"source": 12, "target": 13},
+        {"source": 4, "target": 15},
+        {"source": 6, "target": 4},
+        {"source": 1, "target": 5},
+        {"source": 5, "target": 8},
+        {"source": 15, "target": 8},
+        {"source": 1, "target": 2},
+        {"source": 10, "target": 3},
+        {"source": 10, "target": 9},
+        {"source": 11, "target": 5},
+        {"source": 0,  "target": 6},
+        {"source": 10, "target": 9}
+      ]
 
-      //container holding circle and label
-      let nodes = items.enter().append("g")
-        .attr("class", "node-container")
-        .attr("id", (d,i) => { return "node-container-" + i})
-        .attr("transform", (d,i) => {return "translate(" + [i*50 + 30 + this.d3.randomUniform(-10, 10)(), 60 + this.d3.randomUniform(-10, 100)()] + ")"})
-        .call(drag)
+      let test_simulation = this.d3.forceSimulation()
+        .force("charge", this.d3.forceManyBody(-5).distanceMax(200))
+        .force("center", this.d3.forceCenter(500, 190))
+        .force("link", this.d3.forceLink().distance(40).strength(1).iterations(1).id(function(d) {return d.index}))
 
-      nodes.append("circle")
+      let link = this.svgSel.append("g")
+      .attr("class", "links").selectAll("line")
+       .data(demo_links)
+       .enter().append("line")
+
+      let items = this.svgSel.selectAll("circle").data(this.items, (d) =>  {return d.value })
+        items.exit().remove()
+
+      let node = items.enter().append("g")
+        .attr("class", "node")
+        .append("circle")
           .attr("class", "node-circle")
           .attr("id", (d,i) => { return "node-circle-" + i})
         .merge(items)
           .style("stroke", "black")
-          .attr("r", "8px")
-          .on("mouseover", (d,i) => {
+          .attr("r", radius + "px")
+          .call(this.d3.drag()
+              .on("start", (d) => {
+                 if (!this.d3.event.active) test_simulation.alphaTarget(.2).restart();
+                 d.fx = d.x;
+                 d.fy = d.y;
+               })
+              .on("drag", (d) => {
+                 d.fx = this.d3.event.x;
+                 d.fy = this.d3.event.y;
+               })
+              .on("end",  (d) =>   {
+                 if (!this.d3.event.active) test_simulation.alphaTarget(0);
+                 d.fx = null;
+                 d.fy = null;
+               }))
+               .on("mouseover", (d,i) => {
             if (!this.d3.select("#node-circle-" + i).classed("selected-circle")) {
               this.d3.select("#node-circle-text-" + i).style("opacity", 1)
 
@@ -89,7 +115,7 @@ namespace fibra {
                 this.d3.select("#node-circle-" + j).classed("selected-circle", false)
                 this.d3.select("#node-circle-text-" + j).style("opacity", 0)
                 this.d3.select("#node-circle-text-" + j).style("cursor", "default")
-                this.d3.select("#node-circle-" + j).attr("r", "7px")
+                this.d3.select("#node-circle-" + j).attr("r", radius + "px")
             }
             //selects the clicked circle
             this.d3.select("#node-circle-" + i).classed("selected-circle", true)
@@ -102,20 +128,25 @@ namespace fibra {
             })
           })
 
-          // appends a text label to each node with the name of the person
-          nodes.append("text")
-            .attr("id", (d,i) => { return "node-circle-text-" + i})
-            .attr("class", "node-text")
-            .attr("text-anchor", "middle")
-            .attr("y", 36)
-            .style("opacity", 0)
-            .text((d,i) => { return d.label.value})
-            .on("click", (d,i) => {
-              // on clicking text label, scrolls page down to person's information
-              if (this.d3.select("#node-circle-" + i).classed("selected-circle")) {
-                $('html,body').animate({scrollTop:$('body').height()}, 500)
-              }
-            })
+              test_simulation.nodes(this.items)
+              .on("tick", () => {
+                node
+                  .attr("transform", function(d) {
+                    let x = d.x, y = d.y
+                    if (d.x > svg_width - radius) x = svg_width - radius
+                    if (d.x < radius) x = radius
+                    if (d.y > svg_height - radius) y = svg_height - radius
+                    if (d.y < radius) y = radius
+                     return "translate(" + x + ", " + y + ")";
+                    })
+
+                link
+                  .attr("x1", (d) => {return d.source.x})
+                  .attr("y1", (d) => {return d.source.y})
+                  .attr("x2", (d) => {return d.target.x})
+                  .attr("y2", (d) => {return d.target.y})
+              })
+              test_simulation.force("link").links(demo_links)
 
           //makes the left column pop-out on hover
           this.d3.select("#left-column").on("mouseover", () => {
