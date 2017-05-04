7a29aab3e4cf00aeff31fb491e5b225e16abe9ec
Ethan Jewett <esjewett@gmail.com>
2017-05-04 11:43:34 -0500

    Fix distribution directory path

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   output: {
     filename: '[name]-bundle.js',
-    path: path.join(__dirname + 'dist')
+    path: path.join(__dirname + '/dist')
   },
   module: {
     rules: [
