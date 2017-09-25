c019a9238e88e3e935b9a15236f9c8ae2c671579
Ethan Jewett <esjewett@gmail.com>
2017-09-25 18:19:08 +0200

    Expose state in SearchService

--- app/services/search-service.ts
+++ app/services/search-service.ts
@@ -1,18 +1,24 @@
 'use strict'
 
 import * as angular from 'angular'
+import { INgRedux } from 'ng-redux'
 
 export class SearchService {
 
   private $q: angular.IQService
+  private $ngRedux: INgRedux
 
   constructor(
-    $q: angular.IQService
+    $q: angular.IQService,
+    $ngRedux: INgRedux
   ) {
     this.$q = $q
+    this.$ngRedux = $ngRedux
   }
 
   public searchSources(query: string): angular.IPromise<{}[]> {
+    console.log(this.$ngRedux.getState())
+
     return this.$q.resolve([
       { description: 'Testing testing' },
       { description: 'Blah blah blah' },
