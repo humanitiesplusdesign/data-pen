b42f728ccd2ab79b383043bb241a3511db2f635a
Thomas Ricciardiello <thomas.ricciardiello@uts.edu.au>
2017-10-24 16:19:18 +1100

    Fixed scaling glitches for mobile devices.

--- js/index_type.js
+++ js/index_type.js
@@ -1,19 +1,23 @@
-var loopCheck = true;
+if ($('#about-text').css('text-align') === 'left') {
+  $("#ani-type").text("the world's");
+} else {
+  var loopCheck = true;
 
-var typed = new Typed('#ani-type', {
+  var typed = new Typed('#ani-type', {
     strings: ["the world's", "VIAF's", "Electronic Enlightenment’s", "the Library of Congress’", "Geonames’", "Wikidata’s", "your colleagues’"],
-		typeSpeed: 50,
+    typeSpeed: 50,
     backSpeed: 20,
-		backDelay: 3000,
+    backDelay: 3000,
     smartBackspace: true, // this is a default
-		cursorChar: '',
+    cursorChar: '',
     loop: true
   });
 
-$(window).scroll(function() {
-	if ($('#tagline').visible()) {
-		typed.start();
-	} else {
-		typed.stop();
-	}
-});
+  $(window).scroll(function() {
+    if ($('#tagline').visible()) {
+      typed.start();
+    } else {
+      typed.stop();
+    }
+  });
+}
