e74180d6f9dd2fa50fef2e3a151bfc707004c7ff
Nicole Coleman <cncoleman@gmail.com>
2017-10-25 10:25:16 -0700

    modify tagline (neither knowledge or information really work)

--- js/index_type.js
+++ js/index_type.js
@@ -1,10 +1,10 @@
 if ($('#about-text').css('text-align') === 'left') {
-  $("#ani-type").text("the world's");
+  $("#ani-type").text("linked data sources around the world");
 } else {
   var loopCheck = true;
 
   var typed = new Typed('#ani-type', {
-    strings: ["the world's", "VIAF's", "Electronic Enlightenment’s", "the Library of Congress’", "Geonames’", "Wikidata’s", "your colleagues’"],
+    strings: ["linked data sources around the world", "VIAF", "the Library of Congress", "Geonames", "Wikidata", "your colleagues"],
     typeSpeed: 50,
     backSpeed: 20,
     backDelay: 3000,
