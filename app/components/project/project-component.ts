22e23e4f52bd5b964654eebcb2ea1208ef0ead77
Thomas Ricciardiello <thomas.ricciardiello@uts.edu.au>
2017-11-01 15:12:11 +1100

    Updated projects-view design, added sort form (unplugged).

--- app/components/project/project-component.ts
+++ app/components/project/project-component.ts
@@ -7,58 +7,58 @@ import { INgRedux } from 'ng-redux'
 
 export class ProjectComponentController {
 
-  private actions: any = {}
-  private currentView: string = 'sources'
+	private actions: any = {}
+	private currentView: string = 'sources'
 
-  /* @ngInject */
-  constructor(private projectActionService: ProjectActionService,
-              private itemsService: ItemsService,
-              private $stateParams: any,
-              private $state: any,
-              private $ngRedux: INgRedux) {
-    let unsub1: () => void = $ngRedux.connect(this.mapProjectToActions, null)(this.actions)
-    this.actions.unsubscribe = () => {
-      unsub1()
-    }
+	/* @ngInject */
+	constructor(private projectActionService: ProjectActionService,
+		private itemsService: ItemsService,
+		private $stateParams: any,
+		private $state: any,
+		private $ngRedux: INgRedux) {
+		let unsub1: () => void = $ngRedux.connect(this.mapProjectToActions, null)(this.actions)
+		this.actions.unsubscribe = () => {
+			unsub1()
+		}
 
-    // Put the project from $stateParams onto the state
-    if ($stateParams.id && $stateParams.sparqlEndpoint && $stateParams.graph)
-      if (this.actions.project.id !== $stateParams.id)
-        this.projectActionService.setProject($stateParams.id, $stateParams.sparqlEndpoint, $stateParams.graph)
+		// Put the project from $stateParams onto the state
+		if ($stateParams.id && $stateParams.sparqlEndpoint && $stateParams.graph)
+			if (this.actions.project.id !== $stateParams.id)
+				this.projectActionService.setProject($stateParams.id, $stateParams.sparqlEndpoint, $stateParams.graph)
 
-    if ($stateParams.view) {
-      if (['sources', 'filter', 'active'].indexOf($stateParams.view) !== -1) {
-        this.currentView = $stateParams.view
-      }
-    }
+		if ($stateParams.view) {
+			if (['sources', 'filter', 'active'].indexOf($stateParams.view) !== -1) {
+				this.currentView = $stateParams.view
+			}
+		}
 
-    itemsService.getAllItemsCount().then((count) => this.projectActionService.setAllItemCount(count))
-    itemsService.getFilteredItemsCount().then((count) => this.projectActionService.setFilteredItemCount(count))
-  }
+		itemsService.getAllItemsCount().then((count) => this.projectActionService.setAllItemCount(count))
+		itemsService.getFilteredItemsCount().then((count) => this.projectActionService.setFilteredItemCount(count))
+	}
 
-  private setView(newView: string): void {
-    this.$state.go('project', { view: newView })
-  }
+	private setView(newView: string): void {
+		this.$state.go('project', { view: newView })
+	}
 
-  private dataUsedProportion(): number {
-    return this.currentView === 'filter' ?
-        this.actions.project.filteredItemsCount / this.actions.project.allItemsCount * 100 :
-      this.currentView === 'active' ?
-        this.actions.project.activeItemsCount / this.actions.project.filteredItemsCount * 100 :
-        0
-  }
+	private dataUsedProportion(): number {
+		return this.currentView === 'filter' ?
+			this.actions.project.filteredItemsCount / this.actions.project.allItemsCount * 100 :
+			this.currentView === 'active' ?
+				this.actions.project.activeItemsCount / this.actions.project.filteredItemsCount * 100 :
+				0
+	}
 
-  private mapProjectToActions(state: any): any {
-    return {
-      project: state.frontend.project
-    }
-  }
+	private mapProjectToActions(state: any): any {
+		return {
+			project: state.frontend.project
+		}
+	}
 }
 
 export class ProjectComponent implements angular.IComponentOptions {
-    public template: any = require('./project.pug')()
-    public controller: any = ProjectComponentController
+	public template: any = require('./project.pug')()
+	public controller: any = ProjectComponentController
 }
 
 angular.module('fibra.components.project', [])
-  .component('project', new ProjectComponent())
+	.component('project', new ProjectComponent())
--- app/components/projects-view/projects-view-component.ts
+++ app/components/projects-view/projects-view-component.ts
@@ -1,63 +1,91 @@
 'use strict'
 import * as angular from 'angular'
 
-import {ICitable} from '../../models/citable'
-import {ProjectSourceInfo} from '../project-sources-view/project-sources-view-component'
-import {ProjectService} from '../../services/project-service/project-service'
-import {SocialAuthService} from '../../services/social-auth-service'
+import { ICitable } from '../../models/citable'
+import { ProjectSourceInfo } from '../project-sources-view/project-sources-view-component'
+import { ProjectService } from '../../services/project-service/project-service'
+import { SocialAuthService } from '../../services/social-auth-service'
 import * as CryptoJS from 'crypto-js'
+import retina from 'retinajs'
 
 export class ProjectsViewComponentController implements angular.IComponentController {
 
-  public projectSourceState: {[id: string]: 'loading'|'error'|'ready' } = {}
-  public projects: {[id: string]: ICitable[]} = {}
-  public projectSources: {[id: string]: ProjectSourceInfo} = {}
-  public lang: string
+	public projectSourceState: { [id: string]: 'loading' | 'error' | 'ready' } = {}
+	public projects: { [id: string]: ICitable[] } = {}
+	public projectSources: { [id: string]: ProjectSourceInfo } = {}
+	public lang: string
 
-  public deleteProject(sourceId: string, projectIndex: number, project: ICitable): void {
-    this.projects[sourceId].splice(projectIndex, 1)
-    this.projectService.deleteCitable(this.projectSources[sourceId].updateEndpoint, project)
-  }
+	public deleteProject(sourceId: string, projectIndex: number, project: ICitable): void {
+		this.projects[sourceId].splice(projectIndex, 1)
+		this.projectService.deleteCitable(this.projectSources[sourceId].updateEndpoint, project)
+	}
 
-  /* @ngInject */
-  constructor(private projectService: ProjectService, socialAuthService: SocialAuthService, $localStorage: any) {
-    let projectSources: ProjectSourceInfo[] =  null//$localStorage['projectSources']
-    if (!projectSources || projectSources.length === 0) {
-      projectSources = []
-      //projectSources.push(new ProjectSourceInfo('Private projects in local browser storage', 'local:projects', 'local:projects', 'local:projects', '', 'http://ldf.fi/fibra/rdfstoreJSEndpoint'))
-      // http://localhost:3000/#!/project-sources?sourceId=Private projects in local browser storage&sparqlEndpoint=local:projects&type=http://ldf.fi/fibra/rdfstoreJSEndpoint
-      //projectSources.push(new ProjectSourceInfo('Projects in local Fuseki SPARQL server', 'http://localhost:3030/fibra/sparql', 'http://localhost:3030/fibra/update', 'http://localhost:3030/fibra/data', '', 'http://ldf.fi/fibra/fusekiEndpoint'))
-      // http://localhost:3000/#!/project-sources?sourceId=Projects in local Fuseki SPARQL server&sparqlEndpoint=http://localhost:3030/fibra/sparql&updateEndpoint=http://localhost:3030/fibra/update&graphStoreEndpoint=http://localhost:3030/fibra/data&type=http://ldf.fi/fibra/fusekiEndpoint
-      // projectSources.push(new ProjectSourceInfo('Shared projects', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/update', 'http://ldf.fi/fibra/data', 'http://ldf.fi/fibra/shared-projects/', 'http://ldf.fi/fibra/fusekiEndpointWithTextIndexAndSecoFunctions'))
-      $localStorage['projectSources'] = projectSources
-    }
+	public $postLink(): void {
+		console.log("hello?")
+		retina();
+	}
 
-    if (socialAuthService.isLoggedIn() && !projectSources.some(s => s.id === 'Personal projects')) {
-      let uid: string = CryptoJS.SHA256(socialAuthService.loginState())
-      projectSources.unshift(new ProjectSourceInfo('Projects', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/user/' + uid + '/projects/', 'http://ldf.fi/fibra/fusekiEndpointWithTextIndexAndSecoFunctions'))
-    }
+	/* @ngInject */
+	constructor(private projectService: ProjectService, socialAuthService: SocialAuthService, $localStorage: any, $document: angular.IDocumentService) {
+		let projectSources: ProjectSourceInfo[] = null//$localStorage['projectSources']
+		if (!projectSources || projectSources.length === 0) {
+			projectSources = []
+			//projectSources.push(new ProjectSourceInfo('Private projects in local browser storage', 'local:projects', 'local:projects', 'local:projects', '', 'http://ldf.fi/fibra/rdfstoreJSEndpoint'))
+			// http://localhost:3000/#!/project-sources?sourceId=Private projects in local browser storage&sparqlEndpoint=local:projects&type=http://ldf.fi/fibra/rdfstoreJSEndpoint
+			//projectSources.push(new ProjectSourceInfo('Projects in local Fuseki SPARQL server', 'http://localhost:3030/fibra/sparql', 'http://localhost:3030/fibra/update', 'http://localhost:3030/fibra/data', '', 'http://ldf.fi/fibra/fusekiEndpoint'))
+			// http://localhost:3000/#!/project-sources?sourceId=Projects in local Fuseki SPARQL server&sparqlEndpoint=http://localhost:3030/fibra/sparql&updateEndpoint=http://localhost:3030/fibra/update&graphStoreEndpoint=http://localhost:3030/fibra/data&type=http://ldf.fi/fibra/fusekiEndpoint
+			// projectSources.push(new ProjectSourceInfo('Shared projects', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/update', 'http://ldf.fi/fibra/data', 'http://ldf.fi/fibra/shared-projects/', 'http://ldf.fi/fibra/fusekiEndpointWithTextIndexAndSecoFunctions'))
+			$localStorage['projectSources'] = projectSources
+		}
 
-    projectSources.forEach(source => {
-      this.projectSources[source.id] = source
-      this.projects[source.id] = []
-      this.projectSourceState[source.id] = 'loading'
-      projectService.listProjects(source).then(
-        projects => {
-          this.projectSourceState[source.id] = 'ready'
-          this.projects[source.id] = projects
-        },
-        err => {
-          this.projectSourceState[source.id] = 'error'
-          this.projects[source.id] = err
-        })
-    })
-  }
+		if (socialAuthService.isLoggedIn() && !projectSources.some(s => s.id === 'Personal projects')) {
+			let uid: string = CryptoJS.SHA256(socialAuthService.loginState())
+			projectSources.unshift(new ProjectSourceInfo('Projects', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/sparql', 'http://ldf.fi/fibra/user/' + uid + '/projects/', 'http://ldf.fi/fibra/fusekiEndpointWithTextIndexAndSecoFunctions'))
+		}
+
+		projectSources.forEach(source => {
+			this.projectSources[source.id] = source
+			this.projects[source.id] = []
+			this.projectSourceState[source.id] = 'loading'
+			projectService.listProjects(source).then(
+				projects => {
+					this.projectSourceState[source.id] = 'ready'
+					this.projects[source.id] = projects
+				},
+				err => {
+					this.projectSourceState[source.id] = 'error'
+					this.projects[source.id] = err
+				})
+		})
+	}
 }
 
 export class ProjectsViewComponent implements angular.IComponentOptions {
-    public controller = ProjectsViewComponentController // (new (...args: any[]) => angular.IController) = SelectViewComponentController
-    public template: string = require('./projects-view.pug')()
+	public controller = ProjectsViewComponentController // (new (...args: any[]) => angular.IController) = SelectViewComponentController
+	public template: string = require('./projects-view.pug')()
 }
 
 angular.module('fibra.components.projects-view', [])
-  .component('projectsView', new ProjectsViewComponent())
+	.component('projectsView', new ProjectsViewComponent())
+	.controller('projectsSortController', function($scope) {
+		$scope.item = {
+			"id": "0",
+			"name": "Date Created",
+		};
+
+		$scope.options = [{
+			"id": "0",
+			"description": "Date created.",
+			"name": "Date created"
+		},
+		{
+			"id": "1",
+			"description": "Alphabetical.",
+			"name": "A-Z"
+		},
+		{
+			"id": "2",
+			"description": "Alphabetical (reversed).",
+			"name": "Z-A"
+		}];
+	});
