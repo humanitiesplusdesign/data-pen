ae7929a1148f0899ec9fb075b78942ee834821b6
Tim Busuttil <timothyjbusuttil@gmail.com>
2017-11-09 16:20:52 +1100

    Added unlinked node button (unplugged), restyled typeahead template to match existing, acronymified source names.

--- app/components/active/active-component.ts
+++ app/components/active/active-component.ts
@@ -386,3 +386,12 @@ export class ActiveComponent implements angular.IComponentOptions {
 
 angular.module('fibra.components.active', ['ui.bootstrap', 'fibra.actions.project', 'fibra.services.search-service', 'filearts.dragDrop', 'ui.grid', 'ui.grid.emptyBaseLayer', 'ui.grid.resizeColumns', 'ui.grid.autoResize', 'ui.grid.edit'])
   .component('active', new ActiveComponent())
+  .filter('makeAcronym', function() {
+    return function (item) {
+      if (item.match(' ') != null) {
+        return item.match(/\b([A-Z])/g).join('')
+      } else {
+        return item
+      }
+    };
+  });
