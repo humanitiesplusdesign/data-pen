8bb2957908b74ace80007d293a582b028520094a
Eetu Mäkelä <eetu.makela@iki.fi>
2017-12-06 07:38:59 -0800

    fix webpack dist build

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const GhPagesWebpackPlugin = require('gh-pages-webpack-plugin');
 const WebpackBuildNotifierPlugin = require("webpack-build-notifier");
+const BitBarWebpackProgressPlugin = require("bitbar-webpack-progress-plugin");
 
 const commonConf = require('./webpack.common.config.js');
 commonConf.watch = false
@@ -22,9 +23,14 @@ module.exports = [ Object.assign({
       template: 'app/index.pug'
     }),
     new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common",
+      filename: "common-bundle.js"
+    }),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({'process.env.NODE_ENV':  '"production"'}),
     new webpack.NoEmitOnErrorsPlugin(),
+    new BitBarWebpackProgressPlugin(),
     new WebpackBuildNotifierPlugin({
       title: "Fibra UI Webpack Distribution Build"
     })
